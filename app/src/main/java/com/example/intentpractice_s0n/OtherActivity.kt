31c50a5973a2f2621d0a7da894d6c0520a1ab33e
s0n <shson971029@naver.com>
2022-08-11 19:46:10 +0900

    finish() 로 현재화면 종료, 이전화면 복귀

--- app/src/main/java/com/example/intentpractice_s0n/OtherActivity.kt
+++ app/src/main/java/com/example/intentpractice_s0n/OtherActivity.kt
@@ -1,11 +1,25 @@
 package com.example.intentpractice_s0n
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+
+            /*
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+             */
+
+            finish()  //지금화면 끄기.
+
+        }
     }
 }
