1d78adec0db4d448b5b4bdd9257ff83e19eba1cc
s0n <shson971029@naver.com>
2022-08-11 19:37:38 +0900

    다른 화면으로. UI 작업 진행

--- app/src/main/java/com/example/intentpractice_s0n/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_s0n/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.intentpractice_s0n
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+            //다른 화면(OtherActivity)으로 이동
+
+            val myIntent = Intent(this, OtherActivity::class.java)  //
+            startActivity(myIntent)
+        }
     }
 }
