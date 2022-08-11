c45626e157e277563112eb27bd12c075c9f5c733
s0n <shson971029@naver.com>
2022-08-11 19:41:11 +0900

    Intent 이용해서 다른 화면으로 이동

--- app/src/main/java/com/example/intentpractice_s0n/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_s0n/MainActivity.kt
@@ -13,7 +13,8 @@ class MainActivity : AppCompatActivity() {
         moveToOtherBtn.setOnClickListener {
 
             //다른 화면(OtherActivity)으로 이동
-
+//          val myIntent= Intent(출발지this, 도착액티비티::class.java)  =>     비행기 티켓 발권.(출발지.도착지 명시되어있음)
+//          startActivity(myIntent)  => startActivity 함수 실행 -> 실제 비행기 탑승 (출발)
             val myIntent = Intent(this, OtherActivity::class.java)  //
             startActivity(myIntent)
         }
